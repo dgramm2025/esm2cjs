e88e187e41bc02175cf478c49e2959de9b0bc1ff
Dimitris Grammatikogiannis <grammatikogiannis.d@wideservices.gr>
2025-10-16 13:43:13 +0300

    0.0.2

--- index.mjs
+++ index.mjs
@@ -4,29 +4,72 @@ import fs from 'fs';
 import path from 'path';
 
 import { glob } from 'glob';
-import { rolldown } from 'rolldown';
+import { rollup as rolldown } from 'rollup';
+
+import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
 import addModuleToDefine from './lib/rollup-plugin-add-module-to-define.js';
 
 
 async function main() {
     const files = glob.sync('amd/src/**/*.js');
     
-    for (const file of files) {
-        const output = file.replace('amd/src', 'amd/build');
+    for (const inputFile of files) {
+        const file = inputFile.replace(/\\/g, '/');
+        const output = file.replace('amd/src', 'amd/build').replace('.js', '.min.js');
 
         if (!fs.existsSync(path.dirname(output))) {
             fs.mkdirSync(path.dirname(output), { recursive: true });
         }
 
-        await rolldown(file, output, {
+        const bundler = await rolldown(
+            {
+            input: file,
             plugins: [
+
+                nodeResolve(),
+
+                commonjs(),
+
+                babel({
+                    babelHelpers: 'bundled',
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                            {
+                                useBuiltIns: 'usage',
+                                corejs: '3',
+                                targets: {
+                                    esmodules: true,
+                                }
+                            }
+                        ]
+                    ],
+                    plugins: [
+                        './lib/babel-plugin-transform-amd-to-commonjs.js'
+                    ],
+                    exclude: 'node_modules/**'
+                }),
+
                 addModuleToDefine({
                     define: {
                         'process.env.NODE_ENV': JSON.stringify('production')
                     }
-                })
+                }),
+
+                terser()
             ]
         });
+
+        await bundler.write({
+            file: output,
+            format: 'cjs',
+            sourcemap: false,
+        });
+        console.log(`Built ${output}`);
     }
 }
 
